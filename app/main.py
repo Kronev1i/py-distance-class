ccd65d72364db635918baae62c0e3666b9de2103
unknown <kgoncharenko83@gmail.com>
2026-01-09 18:22:33 +0100

    Passed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other):
+        if hasattr(other, "km"):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other) -> "Distance":
+        if hasattr(other, "km"):
+            self.km += other.km
+        else:
+            self.km += other
+
+        return self
+
+    def __mul__(self, other) -> "Distance":
+        return Distance(self.km * other)
+
+    def __truediv__(self, other) -> "Distance":
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other) -> bool:
+        if hasattr(other, "km"):
+            return self.km < other.km
+
+        return self.km < other
+
+    def __le__(self, other) -> bool:
+        if hasattr(other, "km"):
+            return self.km <= other.km
+
+        return self.km <= other
+
+    def __eq__(self, other) -> bool:
+        if hasattr(other, "km"):
+            return self.km == other.km
+
+        return self.km == other
+
+    def __gt__(self, other) -> bool:
+        if hasattr(other, "km"):
+            return self.km > other.km
+
+        return self.km > other
+
+    def __ge__(self, other) -> bool:
+        if hasattr(other, "km"):
+            return self.km >= other.km
+
+        return self.km >= other
